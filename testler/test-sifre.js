1ae95bcce2bfe298cd224914f777d81385d76fcd
KARANKOYU <262628218+KARANKOYU@users.noreply.github.com>
2026-08-29 12:38:37 +0300

    commit 73

--- testler/test-sifre.js
+++ testler/test-sifre.js
@@ -9,3 +9,10 @@ function kontrol(ad, sart, detay) {
   else { kaldi++; console.log('  KALDI  ' + ad + (detay ? '  -> ' + detay : '')); }
 }
 
+/* Sunucu e-posta ayarli degilken sifirlama anahtarini gunluge yaziyor. */
+function sonAnahtar() {
+  const m = fs.readFileSync(LOG, 'utf8').match(/Anahtar\s*:\s*([a-f0-9]{64})/g);
+  if (!m || !m.length) return '';
+  return m[m.length - 1].match(/([a-f0-9]{64})/)[1];
+}
+
