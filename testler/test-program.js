514c24785c2776baef588ca1ba219b03e646a5ae
KARANKOYU <262628218+KARANKOYU@users.noreply.github.com>
2026-08-29 11:54:37 +0300

    commit 63

---
+++ testler/test-program.js
@@ -0,0 +1,8 @@
+const { iste, girisYap } = require('./giris');
+
+let gecti = 0, kaldi = 0;
+function kontrol(ad, sart, detay) {
+  if (sart) { gecti++; console.log('  GECTI  ' + ad); }
+  else { kaldi++; console.log('  KALDI  ' + ad + (detay ? '  -> ' + detay : '')); }
+}
+
