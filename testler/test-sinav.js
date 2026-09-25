e1cadc73861e7dabef814c127b7d48e2e11336f8
KARANKOYU <262628218+KARANKOYU@users.noreply.github.com>
2026-09-25 21:08:28 +0300

    commit 228

---
+++ testler/test-sinav.js
@@ -0,0 +1,5 @@
+/* Sınavlar: şablonlar, grupsuz sınav, ondalıklı (virgüllü) değer, aralık
+   denetimi, "+ yeni değer ekle", grafik verisi, grup ortalaması.
+   Ödevin açılma zamanı ve "geç yaptı" sonucu da burada. */
+const { iste, girisYap } = require('./giris');
+
