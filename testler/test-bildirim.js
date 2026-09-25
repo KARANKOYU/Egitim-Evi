4d82fd602a3d470f4665e33d8c39da0186fb2bc1
KARANKOYU <262628218+KARANKOYU@users.noreply.github.com>
2026-09-25 22:10:42 +0300

    commit 253

--- testler/test-bildirim.js
+++ testler/test-bildirim.js
@@ -7,3 +7,10 @@
    ikinci kez gitmez. */
 const { iste, girisYap, hesapAc } = require('./giris');
 
+let gecti = 0, kaldi = 0;
+function kontrol(ad, sart, detay) {
+  if (sart) { gecti++; console.log('  GECTI  ' + ad); }
+  else { kaldi++; console.log('  KALDI  ' + ad + (detay ? '  -> ' + detay : '')); }
+}
+const gun = n => { const d = new Date(); d.setDate(d.getDate() + n); return d.toISOString().slice(0, 10); };
+
