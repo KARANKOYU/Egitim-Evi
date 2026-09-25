0a61324b18ef072e65972b883ee8a3aefdcbf2f1
KARANKOYU <262628218+KARANKOYU@users.noreply.github.com>
2026-09-25 21:09:33 +0300

    commit 229

--- testler/test-sinav.js
+++ testler/test-sinav.js
@@ -3,3 +3,10 @@
    Ödevin açılma zamanı ve "geç yaptı" sonucu da burada. */
 const { iste, girisYap } = require('./giris');
 
+let gecti = 0, kaldi = 0;
+function kontrol(ad, sart, detay) {
+  if (sart) { gecti++; console.log('  GECTI  ' + ad); }
+  else { kaldi++; console.log('  KALDI  ' + ad + (detay ? '  -> ' + detay : '')); }
+}
+const kisa = v => JSON.stringify(v).slice(0, 160);
+
