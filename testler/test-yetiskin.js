db0e307d5fb1c4435a8bd0c0632418b5a50cab4e
KARANKOYU <262628218+KARANKOYU@users.noreply.github.com>
2026-09-26 11:20:07 +0300

    commit 342

---
+++ testler/test-yetiskin.js
@@ -0,0 +1,19 @@
+/* Yetişkin hesabı ve okul rolleri:
+   - yetişkin hesabı kaydolur; e-postaya kod (iki adımlı giriş) öğrenci dışında herkese zorunlu;
+   - okul başvurusu "müdür" rol satırı açar, yönetici onaylayana kadar girilemez;
+   - öğretmen eşleme kodunu verir, müdür kodu girince öğretmen rolü açılır; kod tek kullanımlık;
+   - tek rolde doğrudan girilir, birden çok seçenekte seçim ekranı; başkasının rolüne geçilemez;
+   - bir kişi iki okulda rol alabilir (A'da öğretmen, B'de müdür) ve veli olabilir;
+   - öğretmen okuldan ayrılabilir, müdür çıkarabilir; onaylı müdür rolü bırakılamaz;
+   - şifre, kişisel bilgi, telefon bildirimi aboneliği yetişkin hesabınındır;
+   - hesabı silme (KVKK) şifreyle; müdürken silinemez. */
+const crypto = require('crypto');
+const { iste, girisYap, hesapAc, mudurYap, kisilikGec, okulHesabi, tcUret, botCevabi } = require('./giris');
+
+let gecti = 0, kaldi = 0;
+function kontrol(ad, sart, detay) {
+  if (sart) { gecti++; console.log('  GECTI  ' + ad); }
+  else { kaldi++; console.log('  KALDI  ' + ad + (detay ? '  -> ' + detay : '')); }
+}
+const J = x => JSON.stringify(x).slice(0, 240);
+
